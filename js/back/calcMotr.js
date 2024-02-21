27dc99de4b8159ea9adc5bbd07eb1f11c0fdca9b
Gecko228 <ITDeveloperTech@gmail.com>
2024-02-21 15:01:21 +0300

    calculator fix

--- js/back/calcMotr.js
+++ js/back/calcMotr.js
@@ -53,12 +53,11 @@ class CalcReplacer {
       }
    }
 
-   splitDatas(href) {
-      console.log(href);
+   static splitDatas(href) {
       let endHref = href.split("?")[1];
       let params = endHref.split("&");
       let dict = {};
-      for (param of params) {
+      for (let param of params) {
          let splits = param.split("=");
          dict[splits[0]] = splits[1];
       }
@@ -70,6 +69,20 @@ class CalcReplacer {
          let skill_imgs = document.querySelectorAll(".tree-item__img > img");
          for (let imageTag of skill_imgs) {
             imageTag.src = imageTag.src.replace("dbpic/", "dbpic_/").toLowerCase();
+         }
+         let jobCountTags = document.querySelectorAll(".tree-layout__tree-item-count");
+         for (let jobCountTag of jobCountTags) {
+            let jobImgTags = jobCountTag.querySelectorAll(".tree-profs__grid-row-image > img");
+            let isFirst = false;
+            for (let jobImgTag of jobImgTags) {
+               if (isFirst) {
+                  jobImgTag.parentElement.style.display = "none";
+                  continue;
+               }
+               let urlParams = CalcReplacer.splitDatas(jobImgTag.src);
+               jobImgTag.src = `https://static.divine-pride.net/images/jobs/icon_jobs_${urlParams["class"]}.png`;
+               isFirst = true;
+            }
          }
          return;
       }
@@ -84,14 +97,40 @@ class CalcReplacer {
       let spanTags = document.querySelectorAll("a.tree-profs__grid-row-cell");
       for (let spanTag of spanTags) {
          let spanImageTags = spanTag.querySelectorAll(".tree-profs__grid-row-image");
-         for (let i = 0; i < spanImageTags.length; i++) {
-            let spanImageTag = spanTag.children[i];
-            let imageTag = spanImageTag.children[0];
-            imageTag.alt = "";
+         for (let i = 0; i < spanImageTags.length - 1; i++) {
+            spanImageTags[i].style.display = "none";
+         }
+         if (spanImageTags === null) {
+            continue;
+         }
+         let spanImageTag = spanImageTags[spanImageTags.length - 1];
+         let imgTag = spanImageTag.querySelector("img");
+         if (imgTag !== null) {
+            let urlParams = CalcReplacer.splitDatas(imgTag.src);
+            imgTag.src = `https://static.divine-pride.net/images/jobs/icon_jobs_${urlParams["class"]}.png`;
          }
       }
    }
 }
+
+let styles = `
+   .tree-profs__grid-row-image img {
+      min-width: 32px;
+      min-height: 32px;
+      max-width: 32px;
+      max-height: 32px;
+      margin: 48px;
+   }
+   .tree-layout__tree-item-count .tree-profs__grid-row-image img {
+      margin: 0;
+   }
+   .tree-profs__grid-row-image {
+      //padding-top: 20px;
+   }
+`;
+let styleSheet = document.createElement("style");
+styleSheet.innerText = styles;
+document.head.append(styleSheet);
 
 if ((document.readyState == "complete") || (document.readyState == "interactive")) {
    let replace = new CalcReplacer();
